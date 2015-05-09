5ad7874633e5818a6d3f5055c35e4a13add9ede2
Bart Feenstra <bart@mynameisbart.com>
2015-05-09 11:33:24 +0200

    Added a stacked exchange rate provider to delegate to other providers.

---
+++ src/AbstractStackedExchangeRateProvider.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * @file
+ * Contains \BartFeenstra\CurrencyExchange\AbstractStackedExchangeRateProvider.
+ */
+
+namespace BartFeenstra\CurrencyExchange;
+
+/**
+ * Gets exchange rates from multiple exchange rate providers.
+ */
+abstract class AbstractStackedExchangeRateProvider implements ExchangeRateProviderInterface
+{
+
+    /**
+     * Gets the available exchange rate providers.
+     *
+     * @return \BartFeenstra\CurrencyExchange\ExchangeRateProviderInterface[]
+     *   Exchange rate providers are ordered by priority, with the highest
+     *   priority first.
+     */
+    abstract protected function getExchangeRateProviders();
+
+    public function load($sourceCurrencyCode, $destinationCurrencyCode)
+    {
+        if ($sourceCurrencyCode == $destinationCurrencyCode) {
+            return new ExchangeRate(time(), $sourceCurrencyCode,
+              $destinationCurrencyCode, 1);
+        }
+
+        foreach ($this->getExchangeRateProviders() as $exchangeRateProvider) {
+            $rate = $exchangeRateProvider->load($sourceCurrencyCode,
+              $destinationCurrencyCode);
+            if ($rate) {
+                return $rate;
+            }
+        }
+
+        return null;
+    }
+
+    public function loadMultiple(array $currencyCodes)
+    {
+        $exchangeRates = [];
+
+        foreach ($currencyCodes as $sourceCurrencyCode => $destinationCurrencyCodes) {
+            // Include all requested rates as unavailable from the start, so
+            // they are included in the return value, even if they cannot be
+            //loaded later on.
+            $exchangeRates[$sourceCurrencyCode] = array_fill_keys($destinationCurrencyCodes,
+              null);
+
+            // Set rates for identical source and destination currencies.
+            foreach ($destinationCurrencyCodes as $index => $destinationCurrencyCode) {
+                if ($sourceCurrencyCode == $destinationCurrencyCode) {
+                    $exchangeRates[$sourceCurrencyCode][$destinationCurrencyCode] = new ExchangeRate(null,
+                      $sourceCurrencyCode, $destinationCurrencyCode, 1);
+                    // Prevent the rate from being loaded by any exchange rate
+                    // providers.
+                    unset($currencyCodes[$sourceCurrencyCode][$index]);
+                }
+            }
+        }
+
+        foreach ($this->getExchangeRateProviders() as $exchangeRateProvider) {
+            $currencyCodes = array_filter($currencyCodes);
+            if ($currencyCodes) {
+                foreach ($exchangeRateProvider->loadMultiple($currencyCodes) as $sourceCurrencyCode => $destinationCurrencyCodes) {
+                    foreach ($destinationCurrencyCodes as $destinationCurrencyCode => $rate) {
+                        if (!is_null($rate)) {
+                            $exchangeRates[$sourceCurrencyCode][$destinationCurrencyCode] = $rate;
+                            // Prevent the rate from being loaded again by other
+                            // exchange rate providers.
+                            $index = array_search($destinationCurrencyCode,
+                              $currencyCodes[$sourceCurrencyCode]);
+                            unset($currencyCodes[$sourceCurrencyCode][$index]);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        return $exchangeRates;
+    }
+
+}
---
+++ tests/src/AbstractStackedExchangeRateProviderTest.php
@@ -0,0 +1,184 @@
+<?php
+
+/**
+ * @file Contains \BartFeenstra\Tests\CurrencyExchange\AbstractStackedExchangeRateProviderTest.
+ */
+
+namespace BartFeenstra\Tests\currency\Unit;
+
+use BartFeenstra\CurrencyExchange\ExchangeRate;
+
+/**
+ * @coversDefaultClass \BartFeenstra\CurrencyExchange\AbstractStackedExchangeRateProvider
+ */
+class AbstractStackedExchangeRateProviderTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * The class under test.
+     *
+     * @var \BartFeenstra\CurrencyExchange\AbstractStackedExchangeRateProvider|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $sut;
+
+    public function setUp()
+    {
+        $this->sut = $this->getMockForAbstractClass('\BartFeenstra\CurrencyExchange\AbstractStackedExchangeRateProvider');
+    }
+
+    /**
+     * @covers ::load
+     */
+    public function testLoad()
+    {
+        $sourceCurrencyCode = 'EUR';
+        $destinationCurrencyCode = 'NLG';
+        $rate = '2.20371';
+
+        $exchangeRateProviderA = $this->getMock('\BartFeenstra\CurrencyExchange\ExchangeRateProviderInterface');
+        $exchangeRateProviderA->expects($this->once())
+          ->method('load')
+          ->with($sourceCurrencyCode, $destinationCurrencyCode)
+          ->willReturn(null);
+
+        $exchangeRateProviderB = $this->getMock('\BartFeenstra\CurrencyExchange\ExchangeRateProviderInterface');
+        $exchangeRateProviderB->expects($this->once())
+          ->method('load')
+          ->with($sourceCurrencyCode, $destinationCurrencyCode)
+          ->willReturn($rate);
+
+        $exchangeRateProviderC = $this->getMock('\BartFeenstra\CurrencyExchange\ExchangeRateProviderInterface');
+        $exchangeRateProviderC->expects($this->never())
+          ->method('load');
+
+        $this->sut->expects($this->atLeastOnce())
+          ->method('getExchangeRateProviders')
+          ->willReturn([
+            $exchangeRateProviderA,
+            $exchangeRateProviderB,
+            $exchangeRateProviderC
+          ]);
+
+        $this->assertSame($rate,
+          $this->sut->load($sourceCurrencyCode, $destinationCurrencyCode));
+    }
+
+    /**
+     * @covers ::load
+     */
+    public function testLoadWithIdenticalCurrencies()
+    {
+        $sourceCurrencyCode = 'EUR';
+        $destinationCurrencyCode = 'EUR';
+
+        $rate = $this->sut->load($sourceCurrencyCode, $destinationCurrencyCode);
+        $this->assertInstanceOf('\BartFeenstra\CurrencyExchange\ExchangeRate',
+          $rate);
+        $this->assertSame(1, $rate->getRate());
+    }
+
+    /**
+     * @covers ::load
+     */
+    public function testLoadWithoutExchangeRateProviders()
+    {
+        $sourceCurrencyCode = 'foo';
+        $destinationCurrencyCode = 'bar';
+
+        $this->sut->expects($this->atLeastOnce())
+          ->method('getExchangeRateProviders')
+          ->willReturn([]);
+
+        $this->assertnull($this->sut->load($sourceCurrencyCode,
+          $destinationCurrencyCode));
+    }
+
+    /**
+     * @covers ::loadMultiple
+     */
+    public function testLoadMultiple()
+    {
+        $sourceCurrencyCodeA = 'EUR';
+        $destinationCurrencyCodeA = 'NLG';
+        $rateA = '2.20371';
+        $sourceCurrencyCodeB = 'NLG';
+        $destinationCurrencyCodeB = 'EUR';
+        $rateB = '0.453780216';
+
+        // Convert both currencies to each other and themselves.
+        $requested_rates_provider = [
+          $sourceCurrencyCodeA => [
+            $destinationCurrencyCodeA,
+            $sourceCurrencyCodeA
+          ],
+          $sourceCurrencyCodeB => [
+            $destinationCurrencyCodeB,
+            $sourceCurrencyCodeB
+          ],
+        ];
+        // By the time plugin A will be called, the identical source and destination
+        // currencies will have been processed.
+        $requested_rates_plugin_a = [
+          $sourceCurrencyCodeA => [$destinationCurrencyCodeA],
+          $sourceCurrencyCodeB => [$destinationCurrencyCodeB],
+        ];
+        // By the time plugin B will be called, the 'A' source and destination
+        // currencies will have been processed.
+        $requested_rates_plugin_b = [
+          $sourceCurrencyCodeB => [$destinationCurrencyCodeB],
+        ];
+
+        $exchangeRateProviderA = $this->getMock('\BartFeenstra\CurrencyExchange\ExchangeRateProviderInterface');
+        $returnedRatesA = [
+          $sourceCurrencyCodeA => [
+            $destinationCurrencyCodeA => new ExchangeRate(null,
+              $sourceCurrencyCodeA, $destinationCurrencyCodeA, $rateA),
+          ],
+          $sourceCurrencyCodeB => [
+            $destinationCurrencyCodeB => null,
+          ],
+        ];
+        $exchangeRateProviderA->expects($this->once())
+          ->method('loadMultiple')
+          ->with($requested_rates_plugin_a)
+          ->willReturn($returnedRatesA);
+
+        $exchangeRateProviderB = $this->getMock('\BartFeenstra\CurrencyExchange\ExchangeRateProviderInterface');
+        $returnedRatesB = [
+          $sourceCurrencyCodeA => [
+            $destinationCurrencyCodeA => null,
+          ],
+          $sourceCurrencyCodeB => [
+            $destinationCurrencyCodeB => new ExchangeRate(null,
+              $sourceCurrencyCodeA, $destinationCurrencyCodeA, $rateB),
+          ],
+        ];
+        $exchangeRateProviderB->expects($this->once())
+          ->method('loadMultiple')
+          ->with($requested_rates_plugin_b)
+          ->willReturn($returnedRatesB);
+
+        $exchangeRateProviderC = $this->getMock('\BartFeenstra\CurrencyExchange\ExchangeRateProviderInterface');
+        $exchangeRateProviderC->expects($this->never())
+          ->method('loadMultiple');
+
+        $this->sut->expects($this->atLeastOnce())
+          ->method('getExchangeRateProviders')
+          ->willReturn([
+            $exchangeRateProviderA,
+            $exchangeRateProviderB,
+            $exchangeRateProviderC
+          ]);
+
+        $returned_rates = $this->sut->loadMultiple($requested_rates_provider);
+        $this->assertSame($returnedRatesA[$sourceCurrencyCodeA][$destinationCurrencyCodeA],
+          $returned_rates[$sourceCurrencyCodeA][$destinationCurrencyCodeA]);
+        $this->assertSame(1,
+          $returned_rates[$sourceCurrencyCodeA][$sourceCurrencyCodeA]->getRate());
+        $this->assertSame($returnedRatesB[$sourceCurrencyCodeB][$destinationCurrencyCodeB],
+          $returned_rates[$sourceCurrencyCodeB][$destinationCurrencyCodeB]);
+        $this->assertSame(1,
+          $returned_rates[$sourceCurrencyCodeB][$sourceCurrencyCodeB]->getRate());
+    }
+
+}
